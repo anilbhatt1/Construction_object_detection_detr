1e4d5635f8b3d28b18ee27ecbc26ca0ecc6e0184
Anil Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2022-06-23 21:51:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -139,6 +139,7 @@ def main(args):
     ]
     optimizer = torch.optim.AdamW(param_dicts, lr=args.lr,
                                   weight_decay=args.weight_decay)
+    print(f'optimizer gets lr of args.lr {args.lr}')
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.lr_drop)
 
     dataset_train = build_dataset(image_set='train', args=args)
